353b8591c2c4b152b3688dbbf9570fbc69d4b56b
AdMatay <129521224+AdMatay@users.noreply.github.com>
2023-04-21 17:49:54 +0300

    Update israeliQueue.c
    no real changes, but be it we need to change the sata structure from a list to a queue

--- israeliQueue.c
+++ israeliQueue.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>     
 #include "IsraeliQueue.h"
 
-typedef struct person * personPtr; //for cleaner code
-typedef struct funcNode* funcNodePtr;//for cleaner code
-//creating the nodes of people in our list with the relevant values
+
+typedef struct person *personPtr; //for cleaner code
+typedef struct funcNode *funcNodePtr;//for cleaner code
+
 typedef struct person 
 {
   int friendsPassed;
   int enemyHeldBack;
-struct person* next;
-};
+struct person *next;
+}person;
+
+funcNodePtr copyFunc(funcNodePtr); //this function appears later in the code and was added by me to make the code cleaner
+personPtr copyPersonList(personPtr head); //this helper function will create a copy of every person inside the function that coies everything
 
 struct funcNode
 {
@@ -30,9 +34,11 @@ struct IsraeliQueue_t
 IsraeliQueue IsraeliQueueCreate(FriendshipFunction* friendFunctions, ComparisonFunction compFunction, int friendThres, int rivalTres){
   IsraeliQueue queue = (IsraeliQueue)malloc(sizeof(IsraeliQueue_t)); //i think the size allocation here needs to be that of the struct itslef
   //queue->friendshipFunctions = friendFunctions;
+  //our pointers inside the israeliqueue are not pointing to anything which is unddefined beahvior maybe we need to change this
   queue->compare = compFunction;
   queue->friendship_th = friendThres;
   queue->rivalry_th = rivalTres;
+  queue->head=NULL; //so that there are no errors
   if(friendFunctions[0] != NULL)
   {
     queue->funcList = (funcNodePtr)(malloc(sizeof(FuncNode)));
@@ -50,26 +56,43 @@ IsraeliQueue IsraeliQueueCreate(FriendshipFunction* friendFunctions, ComparisonF
 
 IsraeliQueue IsraeliQueueClone(IsraeliQueue q) //mine!!
 {
-  if(q==NULL) //check for nullity
+  if(q==NULL) //check for nullity if so then return null
   {
     return NULL;
   }
-IsraeliQueue clone = (IsraeliQueue)malloc(sizeof(IsraeliQueue_t)); ;
-clone->rivalry_th=q->rivalry_th;
-clone->friendship_th=q->friendship_th;
-clone->compare=q->compare;
-//need to add the pointer to the person
-while(q->funcList->func!=NULL)
-{
-
-
+  IsraeliQueue clone = (IsraeliQueue)malloc(sizeof(IsraeliQueue_t)); 
+  clone->rivalry_th=q->rivalry_th;
+  clone->friendship_th=q->friendship_th;
+  clone->compare=q->compare;
+  //need to add the pointer to the person and the persons info
+  clone->funcList=copyFunc(q->funcList);
+   return clone;
 }
 
-
-//need to deal with friendship function see that it is not trivial because you need to create a new one
-return clone;
-
+funcNodePtr copyFunc(funcNodePtr head) //helper function for cloning linked list of func
+{ //this is an addition to the api
+  if(head==NULL)
+  {
+    return NULL;
+  }
+     funcNodePtr newNode= (funcNodePtr)malloc(sizeof(FuncNode));
+     newNode->func = head->func;
+     newNode->next = copyFunc(head->next);
+     return newNode;
 }
+personPtr copyPerson(personPtr head) //helper function for cloning queue of person data
+{ //this is an addition to the api
+  if(head==NULL)
+  {
+    return NULL;
+  }
+     personPtr person= (personPtr)malloc(sizeof(person));
+     person->enemyHeldBack = head->enemyHeldBack;
+      person->friendsPassed= head->friendsPassed;
+    person->next=copyPerson(head->next);
+     return person;
+}
+
 
 void IsraeliQueueDestroy(IsraeliQueue queue){
   personPtr p = queue->head;
