f59f7e845e3c72048fe37cdfa780e2bed5944e41
AlonArm <alon.armoni@campus.technion.ac.il>
2023-05-01 15:00:17 +0300

    switched exit(0) with return 0

--- main.c
+++ main.c
@@ -5,28 +5,24 @@
 #include "HackerEnrollment.h"
 
 int main(int argc, char** argv){
-    if(argc != 6 && argc != 7){
-        printf("Wrong Syntax! correct syntax is:\n%s\n",
-         "HackEnrollment <flags> <students> <courses> <hackers> <queues> <target>");
-        exit(0);
-    }
-    FILE* target = fopen(argv[argc], "w");
+    if(argc == 6 && argc == 7){
+        FILE* target = fopen(argv[argc], "w");
     if(target == NULL){
         printf("Could not find and open the target file!\n");
-        exit(0);
+        return 0;
     }
     FILE* queues = fopen(argv[argc - 1], "r");
     if(queues == NULL){
         printf("Could not find and open the queues file!\n");
         fclose(target);
-        exit(0);
+        return 0;
     }
     FILE* hackers = fopen(argv[argc - 2], "r");
     if(hackers == NULL){
         printf("Could not find and open the hackers file!\n");
         fclose(target);
         fclose(queues);
-        exit(0);
+        return 0;
     }
     FILE* courses = fopen(argv[argc - 3], "r");
     if(courses == NULL){
@@ -34,7 +30,7 @@ int main(int argc, char** argv){
         fclose(target);
         fclose(queues);
         fclose(hackers);
-        exit(0);
+        return 0;
     }
     FILE* students = fopen(argv[argc - 4], "r");
     if(students == NULL){
@@ -43,7 +39,7 @@ int main(int argc, char** argv){
         fclose(queues);
         fclose(hackers);
         fclose(courses);
-        exit(0);
+        return 0;
     }
     //bool ignoreBigLetters = argc == 7 && !strcmp(argv[1], "-i");
     //ignoreBigLetters needs to be added to the enrollment system
@@ -62,4 +58,11 @@ int main(int argc, char** argv){
     fclose(hackers);
     fclose(courses);
     fclose(students);
+    }
+    else{
+        printf("Wrong Syntax! correct syntax is:\n%s\n",
+         "HackEnrollment <flags> <students> <courses> <hackers> <queues> <target>");
+
+    }
+    return 0;
 }
