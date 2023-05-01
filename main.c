92b35c4ad7c802a97c9fc027146d66886a94c865
AlonArm <alon.armoni@campus.technion.ac.il>
2023-05-01 14:05:46 +0300

    fixed build errors in main.c

--- main.c
+++ main.c
@@ -8,25 +8,25 @@ int main(int argc, char** argv){
     if(argc != 6 && argc != 7){
         printf("Wrong Syntax! correct syntax is:\n%s\n",
          "HackEnrollment <flags> <students> <courses> <hackers> <queues> <target>");
-        exit;
+        exit(0);
     }
     FILE* target = fopen(argv[argc], "w");
     if(target == NULL){
         printf("Could not find and open the target file!\n");
-        exit;
+        exit(0);
     }
     FILE* queues = fopen(argv[argc - 1], "r");
     if(queues == NULL){
         printf("Could not find and open the queues file!\n");
         fclose(target);
-        exit;
+        exit(0);
     }
     FILE* hackers = fopen(argv[argc - 2], "r");
     if(hackers == NULL){
         printf("Could not find and open the hackers file!\n");
         fclose(target);
         fclose(queues);
-        exit;
+        exit(0);
     }
     FILE* courses = fopen(argv[argc - 3], "r");
     if(courses == NULL){
@@ -34,7 +34,7 @@ int main(int argc, char** argv){
         fclose(target);
         fclose(queues);
         fclose(hackers);
-        exit;
+        exit(0);
     }
     FILE* students = fopen(argv[argc - 4], "r");
     if(students == NULL){
@@ -43,12 +43,13 @@ int main(int argc, char** argv){
         fclose(queues);
         fclose(hackers);
         fclose(courses);
-        exit;
+        exit(0);
     }
-    bool ignoreBigLetters = argc == 7 && !strcmp(argv[1], "-i");
-    EnrollmentSystem* system = createEnrollment(students, courses, hackers);
+    //bool ignoreBigLetters = argc == 7 && !strcmp(argv[1], "-i");
+    //ignoreBigLetters needs to be added to the enrollment system
+    EnrollmentSystem system = createEnrollment(students, courses, hackers);
     if(system != NULL){
-        EnrollmentSystem* queuedSystem = readEnrollment(system, queues);
+        EnrollmentSystem queuedSystem = readEnrollment(system, queues);
         if(queuedSystem != NULL){
             hackEnrollment(queuedSystem, target);
             printf("Enrollment system was changed successfully");
