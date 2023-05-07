0faf913758b6ef39937d9d6c08c58bcd52d75664
AdMatay <129521224+AdMatay@users.noreply.github.com>
2023-05-07 17:31:32 +0300

    added documentation and fixed ifs{} to match convention

--- tool/HackerEnrollment.c
+++ tool/HackerEnrollment.c
@@ -2,35 +2,39 @@
 #include <stdlib.h> 
 #include <string.h>    
 
-int friendsByHackerFile(void*, void*);
-int friendsByName(void*, void*);
-int friendByID(void*, void*);
-int nameDiff(char*, char*);
-int compareStudents(void*, void*);
+int friendsByHackerFile(void*, void*); //checks the friendship status of two students based on hacker file
+int friendsByName(void*, void*); //checks the friendship status of two students based on their name
+int friendByID(void*, void*); //checks the friendship status of two students based on their id
+int nameDiff(char*, char*); //returns the ascii integer difference between two strings
+int compareStudents(void*, void*); //compares to see if two students ids are the same
 
-char* readLine(FILE*);
-char* getWord(char*);
-bool isEmpty(FILE*);
+char* readLine(FILE*); //reads an entire line from a file and returns a string
+char* getWord(char*); //reads a string and returns the first word before a space or the end of the string
+bool isEmpty(FILE*); //checks if a file is empty
 
-struct Student* createStudent(char*);
-struct Course* createCourse(char*);
-struct Hacker* createHacker(char*, char*, char*);
+struct Student* createStudent(char*); //creates the student struct
+struct Course* createCourse(char*); //creates the course struct
+struct Hacker* createHacker(char*, char*, char*); //creeates the hacker struct
 
-void destroyList(struct Node*, void(void*));
-void destroyStudent(void*);
-void destroyCourse(void*);
-void destroyHacker(void*);
-void destroyNodes(void*);
+void destroyList(struct Node*, void(void*)); //destroys a list and the data it holds
+void destroyStudent(void*); //destroys a student struct
+void destroyCourse(void*); //destroys a course struct
+void destroyHacker(void*); //destroys a hacker struct
+void destroyNodes(void*); //destroys a node 
 
-struct Student* findStudent(struct Node*, char*);
-struct Course* findCourse(struct Node*, char*);
+struct Student* findStudent(struct Node*, char*); //finds a student in a list of students
+struct Course* findCourse(struct Node*, char*); //finds a course in a list of courses
 
-int posInQueue(IsraeliQueue, void*);
+int posInQueue(IsraeliQueue, void*); //returns the positin of a node inside a queue
 
 int friendsByHackerFile(void* hacker, void* student){
-    if(hacker == NULL || student == NULL) return 0;
+    if(hacker == NULL || student == NULL){
+        return 0;
+    }
     struct Student* hr = (struct Student*)hacker;
-    if(hr->hackerInfo == NULL) return 0;
+    if(hr->hackerInfo == NULL){ 
+        return 0;
+    }
     char* studentID = ((struct Student*)student)->ID;
     
     Node* temp = hr->hackerInfo->friends;
@@ -50,9 +54,13 @@ int friendsByHackerFile(void* hacker, void* student){
     return 0;
 }
 int friendsByName(void* hr, void* st){
-    if(hr == NULL || st == NULL) return 0;
+    if(hr == NULL || st == NULL) {
+        return 0;
+    }
     struct Student* hacker = (struct Student*)hr;
-    if(hacker->hackerInfo == NULL) return 0;
+    if(hacker->hackerInfo == NULL){ 
+        return 0;
+    }
     struct Student* student = (struct Student*)st;
     return nameDiff(hacker->name, student->name)+nameDiff(hacker->surName, student->surName);
 }
@@ -60,9 +68,13 @@ int nameDiff(char* name1, char* name2){
     int sum = 0;
     int i = 0;
     while(name1[i] != '\0' && name2[i] != '\0'){
-        if(name1[i] > name2[i]) sum += name1[i] - name2[i];
-        else sum += name2[i] - name1[i];
-        i++;
+        if(name1[i] > name2[i]){ 
+            sum += name1[i] - name2[i];
+        }
+        else{
+             sum += name2[i] - name1[i];
+            i++;
+        }
     }
     char* name = (name1[i] != '\0') ? name1 : name2;
     while(name[i] != '\0'){
@@ -72,7 +84,9 @@ int nameDiff(char* name1, char* name2){
     return sum;
 }
 int friendByID(void* hr, void* st){
-    if(hr == NULL || st == NULL || ((struct Student*)hr)->hackerInfo == NULL) return 0;
+    if(hr == NULL || st == NULL || ((struct Student*)hr)->hackerInfo == NULL){
+        return 0;
+    }
     int id1 = atoi(((struct Student*)hr)->ID);
     int id2 = atoi(((struct Student*)st)->ID);
     if(id2 > id1) return id2 - id1;
@@ -87,7 +101,9 @@ int compareStudents(void* st1, void* st2){
 }
 
 char* readLine(FILE* file){
-    if(file == NULL) return NULL;
+    if(file == NULL){
+         return NULL;
+    }
     long pos = ftell(file);
     int counter = 0;
     char c;
@@ -97,7 +113,9 @@ char* readLine(FILE* file){
     }while(c != '\n' && c != EOF);
     fseek(file, pos, SEEK_SET);
     char* line = (char*)malloc(sizeof(char) * (counter + 1));
-    if(line == NULL) return NULL;
+    if(line == NULL){
+        return NULL;
+    }
     char* temp = fgets(line, counter + 1, file);
     if(temp == NULL){
         free(line);
@@ -107,14 +125,25 @@ char* readLine(FILE* file){
     return line;
 }
 char* getWord(char* line){
-    if(line == NULL || line[0] == '\0') return NULL;
+    if(line == NULL || line[0] == '\0'){
+        return NULL;
+    }
     int i = 0;
-    while(line[i] != '\0' && line[i] != ' ') i++;
-    if(i == 0) return NULL;
+    while(line[i] != '\0' && line[i] != ' ') {
+        i++;
+    }
+    if(i == 0) {
+        return NULL;
+    }
     line[i] = '\0';
     char* newWord = (char*)malloc((i + 1) * sizeof(char));
-    if(newWord == NULL) return NULL;
-    for(int j = 0 ; j <= i ; j++) newWord[j] = line[j];
+    if(newWord == NULL)
+    { 
+        return NULL;
+    }
+    for(int j = 0 ; j <= i ; j++) {
+        newWord[j] = line[j];
+    }
     int j = 0;
     while(line[j + i + 1] != '\0'){
         line[j] = line[j + i + 1];
@@ -134,9 +163,13 @@ bool isEmpty(FILE* file){
     return isEmpty;
 }
 struct Student* createStudent(char* info){
-    if(info == NULL) return NULL;
+    if(info == NULL){
+        return NULL;
+    }
     struct Student* st = (struct Student*)malloc(sizeof(struct Student));
-    if(st == NULL) return NULL;
+    if(st == NULL) {
+        return NULL;
+    }
     st->ID = getWord(info);
     st->totalCredits = getWord(info);
     st->GPA = getWord(info);
@@ -148,9 +181,13 @@ struct Student* createStudent(char* info){
     return st;
 }
 struct Course* createCourse(char* info){
-    if(info == NULL) return NULL;
+    if(info == NULL) {
+        return NULL;
+    }
     struct Course* course = (struct Course*)malloc(sizeof(struct Course));
-    if(course == NULL) return NULL;
+    if(course == NULL) {
+        return NULL;
+    }
     FriendshipFunction functions[4] = {friendsByHackerFile, friendsByName, friendByID, NULL};
     course->queue = IsraeliQueueCreate(functions, compareStudents, FRIENDSHIP_TH, ENEMY_TH);
     course->courseNum = getWord(info);
@@ -161,7 +198,9 @@ struct Course* createCourse(char* info){
 }
 struct Hacker* createHacker(char* courses, char* friends, char* rivals){
     struct Hacker* hacker = (struct Hacker*)malloc(sizeof(struct Hacker));
-    if(hacker == NULL) return NULL;
+    if(hacker == NULL) {
+        return NULL;
+    }
     hacker->desiredCourses = NULL;
     hacker->friends = NULL;
     hacker->rivals = NULL;
@@ -208,7 +247,9 @@ struct Hacker* createHacker(char* courses, char* friends, char* rivals){
 }
 
 void destroyStudent(void* student){
-    if(student == NULL) return;
+    if(student == NULL) {
+        return;
+    }
     struct Student* st = (struct Student*)student;
     free(st->ID);
     free(st->totalCredits);
@@ -221,14 +262,18 @@ void destroyStudent(void* student){
     free(st);
 }
 void destroyCourse(void* course){
-    if(course == NULL) return;
+    if(course == NULL) {
+        return;
+    }
     struct Course* cs = (struct Course*)course;
     free(cs->courseNum);
     IsraeliQueueDestroy(cs->queue);
     free(cs);
 }
 void destroyHacker(void* hacker){
-    if(hacker == NULL) return;
+    if(hacker == NULL) {
+        return;
+    }
     struct Hacker* hr = (struct Hacker*)hacker;
     destroyList(hr->desiredCourses, free);
     destroyList(hr->friends, free);
@@ -236,13 +281,17 @@ void destroyHacker(void* hacker){
     free(hacker);
 }
 void destroyList(struct Node* list, void(*destroyFunc)(void*)){
-    if(list == NULL) return;
+    if(list == NULL){
+        return;
+    }
     (destroyFunc)(list->data);
     destroyList(list->next, destroyFunc);
     free(list);
 }
 void destroyNodes(void* node){
-    if(node == NULL) return;
+    if(node == NULL){
+        return;
+    }
     struct Node* temp = (struct Node*)node;
     destroyNodes(temp->next);
     free(node);
@@ -255,22 +304,30 @@ void destroySystem(EnrollmentSystem sys){
 }
 
 void changeBigLetters(EnrollmentSystem sys){
-    if(sys == NULL) return;
+    if(sys == NULL){
+        return;
+    }
     struct Node* iterator = sys->studentsList;
     while(iterator != NULL){
         char* name = ((struct Student*)iterator->data)->name;
         for(int i = 0 ; name[i] != '\0' ; i++){
-            if(name[i] >= 'A' && name[i] <= 'Z') name[i] += 'a'-'A';
+            if(name[i] >= 'A' && name[i] <= 'Z'){
+                name[i] += 'a'-'A';
+            }
         }
         char* surname = ((struct Student*)iterator->data)->surName;
         for(int i = 0 ; surname[i] != '\0' ; i++){
-            if(surname[i] >= 'A' && surname[i] <= 'Z') surname[i] += 'a' - 'A';
+            if(surname[i] >= 'A' && surname[i] <= 'Z'){
+                surname[i] += 'a' - 'A';
+            }
         }
         iterator = iterator->next;
     }
 }
 int posInQueue(IsraeliQueue queue, void* item){
-    if(queue == NULL || item == NULL) return -1;
+    if(queue == NULL || item == NULL){
+        return -1;
+    }
     IsraeliQueue tempQueue = IsraeliQueueClone(queue);
     int pos = 0;
     while(IsraeliQueueSize(tempQueue) > 0){
@@ -287,23 +344,31 @@ int posInQueue(IsraeliQueue queue, void* item){
 }
 
 struct Student* findStudent(struct Node* studentList, char* ID){
-    if(ID == NULL || studentList == NULL) return NULL;
+    if(ID == NULL || studentList == NULL){
+        return NULL;
+    }
     if(strcmp(ID, ((struct Student*)studentList->data)->ID) == 0){
         return (struct Student*)(studentList->data);
     }
     return findStudent(studentList->next, ID);
 }
 struct Course* findCourse(struct Node* coursesList, char* courseNum){
-    if(courseNum == NULL || coursesList == NULL) return NULL;
+    if(courseNum == NULL || coursesList == NULL){
+        return NULL;
+    }
     if(strcmp(courseNum, ((struct Course*)coursesList->data)->courseNum) == 0){
         return (struct Course*)(coursesList->data);
     }
     return findCourse(coursesList->next, courseNum);
 }
 EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers){
-    if(students == NULL || courses == NULL || hackers == NULL) return NULL;
+    if(students == NULL || courses == NULL || hackers == NULL){
+        return NULL;
+    }
     EnrollmentSystem system = (EnrollmentSystem)malloc(sizeof(struct EnrollmentSystem_t));
-    if(system == NULL) return NULL;
+    if(system == NULL){
+        return NULL;
+    }
     system->coursesList = NULL;
     system->hackers = NULL;
     system->studentsList = NULL;
@@ -380,10 +445,14 @@ EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers){
     return system;
 }
 EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues){
-    if(sys == NULL || queues == NULL) return NULL;
+    if(sys == NULL || queues == NULL) {
+        return NULL;
+    }
     while(!isEmpty(queues)){
         char* line = readLine(queues);
-        if(line == NULL) return NULL;
+        if(line == NULL){
+            return NULL;
+        }
         char* courseNum = getWord(line);
         struct Course* course = findCourse(sys->coursesList, courseNum);
         free(courseNum);
@@ -410,14 +479,18 @@ EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues){
     return sys;
 }
 void hackEnrollment(EnrollmentSystem sys, FILE* out){
-    if(sys == NULL || out == NULL) return;
+    if(sys == NULL || out == NULL){
+        return;
+    }
     struct Node* iterator = sys->hackers;
     while(iterator != NULL){
         struct Student* st = (struct Student*)(iterator->data);
         struct Node* courseI = st->hackerInfo->desiredCourses;
         while(courseI != NULL){
             struct Course* course = findCourse(sys->coursesList, (char*)(courseI->data));
-            if(course == NULL) return;
+            if(course == NULL){
+                return;
+            }
             IsraeliQueueEnqueue(course->queue, st);
             courseI = courseI->next;
         }
@@ -433,8 +506,12 @@ void hackEnrollment(EnrollmentSystem sys, FILE* out){
             struct Course* course = findCourse(sys->coursesList, (char*)desiredCourses->data);
             int pos = posInQueue(course->queue, tmp);
             int size = course->size;
-            if(pos < size) acceptedCount++;
-            else rejectedCount++;
+            if(pos < size){
+                acceptedCount++;
+            }
+            else{
+                rejectedCount++;
+            }
             desiredCourses = desiredCourses->next;
         }
         if(acceptedCount < 2 && rejectedCount > 0){
@@ -479,7 +556,9 @@ void printSystem(EnrollmentSystem sys){
         student->ID, student->totalCredits, student->GPA, student ->name,
         student->surName, student->city, student->department);
         struct Hacker* hackerInfo = student->hackerInfo;
-        if(hackerInfo == NULL) printf("not a hacker\n");
+        if(hackerInfo == NULL) {
+            printf("not a hacker\n");
+        }
         else{
             printf("desired courses: ");
             struct Node* temp1 = hackerInfo->desiredCourses;
